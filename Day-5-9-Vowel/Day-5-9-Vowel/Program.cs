594e54c178ef934b61107d8994da85869cf72b7b
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-17 23:11:02 +0530

    Vowel file added

---
+++ Day-5-9-Vowel/Day-5-9-Vowel/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter an Alphabet");
+char alp = Convert.ToChar(Console.ReadLine());
+if (alp == 'a'  || alp == 'A' || alp == 'e'|| alp == 'E' || alp == 'i' || alp == 'I' ||
+    alp == 'o' || alp == 'O' || alp == 'u' || alp == 'U')
+{
+    Console.WriteLine("Enterd Alphabet is a VOWEL");
+
+}
+else
+    Console.WriteLine("Entered Alphabet is consonant");
