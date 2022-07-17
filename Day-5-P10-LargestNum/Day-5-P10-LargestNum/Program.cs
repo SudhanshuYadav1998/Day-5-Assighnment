a2881b325dc212e003c0db5d3a929a0cf41fd14a
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 01:06:56 +0530

    Added largest number file

---
+++ Day-5-P10-LargestNum/Day-5-P10-LargestNum/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the first Number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Second Number b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Third Number c");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine("A is largest");
+}
+else if (c > b && c > a)
+{
+    Console.WriteLine("C is largest");
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine("B is Largest");
+}
+else
+    Console.WriteLine("Numbers are Equal");
