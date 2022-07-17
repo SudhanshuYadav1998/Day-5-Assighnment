94061f0eb075ebfb07369651ef58ccb6ad529d39
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-17 22:59:19 +0530

    Added even odd program

---
+++ Day-5-8-EvenOdd/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the Number");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine("Number is Even");
+}
+else
+    Console.WriteLine("Number is Odd");
