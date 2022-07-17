89ea4725096380db16e293e8590536f23af37106
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-17 23:14:09 +0530

    Added Coin flip file

---
+++ Day-5-CoinFlip/Day-5-CoinFlip/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Coin Flip Count");
+double Toss = Convert.ToDouble(Console.ReadLine());
+int Hcount = 0;
+int Tcount = 0;
+for (int i = 0; i < Toss; i++)
+{
+    Random random = new Random();
+    int dice = random.Next(0, 2);
+    if (dice < .5)
+    {
+        Hcount += 1;
+    }
+    else
+        Tcount += 1;
+}
+Console.WriteLine("Head count: " + Hcount);
+Console.WriteLine("Tail count: " + Tcount);
+double Hpercent = ((double)Hcount / Toss) * 100;
+Console.WriteLine("Head percentage is: " + Hpercent);
+double Tpercent = (((double)Tcount / Toss)) * 100;
+Console.WriteLine("Tail percentage is: " + Tpercent);
+
