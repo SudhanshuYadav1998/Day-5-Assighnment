2b1eeb83158954849bdf07a0497ad84051472116
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 00:43:01 +0530

    Added prime factors file

---
+++ Day-5-Factors/Day-5-Factors/Program.cs
@@ -0,0 +1,17 @@
+﻿
+    Console.WriteLine("Enter the Number to calculate Prime Factors:");
+    int Number = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 2; i <= Number / 2; i++)
+    {
+        while (Number % i == 0)
+        {
+            Console.WriteLine(i);
+            Number = Number / i;
+        }
+    }
+    if (Number > 2)
+    {
+        Console.WriteLine(Number);
+    }
+
