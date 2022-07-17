088dd5f137c9e6fb3a965781f7480499f91720cf
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 01:56:11 +0530

    added powers of two file

---
+++ Powers-2/Powers-2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter power of 2");
+int N=Convert.ToInt32(Console.ReadLine());
+if (N>0 && N<32)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        double value = Math.Pow(2, i);
+        Console.WriteLine(value);
+    }
+}
+else
+    Console.WriteLine("Invalid Number");
