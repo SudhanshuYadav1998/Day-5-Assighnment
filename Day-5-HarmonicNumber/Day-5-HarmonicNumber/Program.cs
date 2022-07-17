91734f52ed0dd280a44c8d765f2ea86f7a726bc2
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 00:45:46 +0530

    Added Harmonic Number file

---
+++ Day-5-HarmonicNumber/Day-5-HarmonicNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the value of N");
+int N=Convert.ToInt32(Console.ReadLine());
+double s = 0;
+if(N>0)
+{
+    for (double i = 1; i <= N; i++)
+    {
+        double Hnum = (1/i);
+        s+=Hnum;
+        Console.WriteLine("The Harmonic series are as follow: " +Hnum.ToString());
+        //Console.WriteLine(s);
+    }
+   // Console.WriteLine(Hnum.ToString());
+    Console.WriteLine("The SUM of above series is: " +s);
+}
+else
+    Console.WriteLine("Invalid Entry");
